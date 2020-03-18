4d2dac569a5f21981956bfd507945b4feeb78861
CloudWorks <>
2020-03-18 16:40:29 +0200

    configured settings storyboard

---
+++ RSSFeedNewsParser/RSSFeedNewsParser/NewsFeed.swift
@@ -0,0 +1,13 @@
+//
+//  NewsFeed.swift
+//  RSSFeedNewsParser
+//
+//  Created by CloudWorks on 18/03/2020.
+//  Copyright © 2020 CloudWorks. All rights reserved.
+//
+
+import Foundation
+
+class NewsFeed {
+    
+}
